68a25e8b61f43e37b87eff0fbbae861f7b5ac70e
Will160714 <95445527+Will160714@users.noreply.github.com>
2024-04-07 22:06:48 -0400

    Added functionality for adding new debit, resetting the input fields, and adding to the debitList

--- src/components/Debits.js
+++ src/components/Debits.js
@@ -4,6 +4,7 @@ src/components/Debits.js
 The Debits component contains information for Debits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
+import { useState } from 'react';
 import {Link} from 'react-router-dom';
 
 const Debits = (props) => {
@@ -15,16 +16,35 @@ const Debits = (props) => {
       return <li key={debit.id}>{debit.amount} {debit.description} {date}</li>
     });
   }
+
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.addDebit(description, amount);
+    setDescription('');
+    setAmount('');
+  }
+
+  const updateDescription = (event) => {
+    setDescription(event.target.value);
+  }
+
+  const updateAmount = (event) => {
+    setAmount(event.target.value);
+  }
+
   // Render the list of Debit items and a form to input new Debit item
   return (
     <div>
       <h1>Debits</h1>
-
+      <h1>{props.balance}</h1>
       {debitsView()}
 
-      <form onSubmit={props.addDebit}>
-        <input type="text" name="description" />
-        <input type="number" name="amount" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="description" value = {description} onChange = {updateDescription}/>
+        <input type="text" name="amount" value = {amount} onChange = {updateAmount}/>
         <button type="submit">Add Debit</button>
       </form>
       <br/>
